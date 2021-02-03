a82c111205de0e65ca0ec6b322953647bb3baebd
Kai Prenger <kai.prenger@gmail.com>
2021-02-02 20:47:37 -0500

    square function, hardcoded and prompting the user

--- tutorial1_getting_started/main.js
+++ tutorial1_getting_started/main.js
@@ -4,4 +4,10 @@ function square(number){
     return number * number
 }
 
-var my_number = 
+var my_number = 5
+
+console.log(square(my_number) + " is " + my_number + " squared!")
+
+num_to_square = prompt("Pick a number?")
+
+console.log("The square of " + num_to_square + " is " + square(num_to_square))
