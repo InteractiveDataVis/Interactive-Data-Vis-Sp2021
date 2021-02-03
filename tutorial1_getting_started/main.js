ca533c039f34dfb069b1ca18e83a5b1984f2bd33
Kai Prenger <kai.prenger@gmail.com>
2021-02-03 07:55:20 -0500

    Add line to console to explain where our square function is used.

--- tutorial1_getting_started/main.js
+++ tutorial1_getting_started/main.js
@@ -11,3 +11,5 @@ console.log(square(my_number) + " is " + my_number + " squared!")
 num_to_square = prompt("Pick a number?")
 
 console.log("The square of " + num_to_square + " is " + square(num_to_square))
+
+console.log("the last two lines printed to the console use a function!")
