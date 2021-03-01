5b51c2d00f2b52e09ef1dd0e005e7877640965ac
auchers <aucher.serr@gmail.com>
2021-03-01 14:45:55 -0500

    update tutorial4 demo code

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -1,14 +1,25 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 },
-  radius = 5;
+  margin = { top: 20, bottom: 50, left: 60, right: 60 },
+  radius = 3;
+
+/*
+this extrapolated function allows us to replace the "G" with "B" min the case of billions.
+we cannot do this in the .tickFormat() because we need to pass a function as an argument,
+and replace needs to act on the text (result of the function).
+*/
+const formatBillions = (num) => d3.format(".2s")(num).replace(/G/, 'B')
+const formatDate = d3.timeFormat("%Y")
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
 let svg;
 let xScale;
 let yScale;
+let yAxis;
+let xAxisGroup;
+let yAxisGroup;
 
 /* APPLICATION STATE */
 let state = {
@@ -18,42 +29,87 @@ let state = {
 
 /* LOAD DATA */
 // + SET YOUR DATA PATH
-d3.json(YOUR_DATA_PATH, d3.autoType).then(raw_data => {
-  console.log("raw_data", raw_data);
-  state.data = raw_data;
-  init();
-});
+d3.csv('../data/populationOverTime.csv', d => {
+  // use custom initializer to reformat the data the way we want it
+  // ref: https://github.com/d3/d3-fetch#dsv
+  return {
+    year: new Date(+d.Year, 0, 1),
+    country: d.Entity,
+    population: +d.Population
+  }
+})
+  .then(data => {
+    console.log("loaded data:", data);
+    state.data = data;
+    init();
+  });
 
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
 function init() {
   // + SCALES
+  xScale = d3.scaleTime()
+    .domain(d3.extent(state.data, d => d.year))
+    .range([margin.right, width - margin.left])
+
+  yScale = d3.scaleLinear()
+    .domain(d3.extent(state.data, d => d.population))
+    .range([height - margin.bottom, margin.top])
 
   // + AXES
+  const xAxis = d3.axisBottom(xScale)
+    .ticks(6) // limit the number of tick marks showing -- note: this is approximate
+  yAxis = d3.axisLeft(yScale)
+    .tickFormat(formatBillions)
 
   // + UI ELEMENT SETUP
+  const selectElement = d3.select("#dropdown")
 
-  const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-    state.selection = this.value; // + UPDATE STATE WITH YOUR SELECTED VALUE
-    console.log("new value is", this.value);
+  // add in dropdown options from the unique values in the data
+  selectElement.selectAll("option")
+    .data([
+      // manually add the first value
+      "Select a country",
+      // add in all the unique values from the dataset
+      ...new Set(state.data.map(d => d.country))])
+    .join("option")
+    .attr("attr", d => d)
+    .text(d => d)
+
+  // + SET SELECT ELEMENT'S DEFAULT VALUE (optional)
+  selectElement.on("change", event => {
+    state.selection = event.target.value
+    console.log('state has been updated to: ', state)
     draw(); // re-draw the graph based on this new selection
   });
 
-  // add in dropdown options from the unique values in the data
-  selectElement
-    .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD DATA VALUES FOR DROPDOWN
-    .join("option")
-    .attr("value", d => d)
-    .text(d => d);
-
-  // + SET SELECT ELEMENT'S DEFAULT VALUE (optional)
-
   // + CREATE SVG ELEMENT
+  svg = d3.select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
   // + CALL AXES
+  xAxisGroup = svg.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${0}, ${height - margin.bottom})`)
+    .call(xAxis)
+
+  xAxisGroup.append("text")
+    .attr("class", 'xLabel')
+    .attr("transform", `translate(${width / 2}, ${35})`)
+    .text("Year")
+
+  yAxisGroup = svg.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.right}, ${0})`)
+    .call(yAxis)
+
+  yAxisGroup.append("text")
+    .attr("class", 'yLabel')
+    .attr("transform", `translate(${-45}, ${height / 2})`)
+    .attr("writing-mode", 'vertical-rl')
+    .text("Population")
 
   draw(); // calls the draw function
 }
@@ -62,20 +118,59 @@ function init() {
 // we call this everytime there is an update to the data/state
 function draw() {
   // + FILTER DATA BASED ON STATE
-  //
+  const filteredData = state.data
+    .filter(d => d.country === state.selection)
+
   // + UPDATE SCALE(S), if needed
-  //
+  yScale.domain([0, d3.max(filteredData, d => d.population)])
   // + UPDATE AXIS/AXES, if needed
-  //
-  // + DRAW CIRCLES, if you decide to
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
-  //
-  // + DRAW LINE AND AREA
+  yAxisGroup
+    .transition()
+    .duration(1000)
+    .call(yAxis.scale(yScale))// need to udpate the scale
+
+  // + DRAW CIRCLES/LABEL GROUPS, if you decide to
+  const dots = svg
+    .selectAll(".dot")
+    .data(filteredData, d => d.year)
+    .join(
+      enter => enter.append("g")
+        .attr("class", "dot")
+        .attr("transform", d => `translate(${xScale(d.year)}, ${yScale(d.population)})`),
+      update => update
+        .call(update => update.transition()
+          .duration(1000)
+          .attr("transform", d => `translate(${xScale(d.year)}, ${yScale(d.population)})`)
+        ),
+      exit => exit.remove()
+    );
+
+  // add circle into group
+  dots.selectAll("circle")
+    .data(d => [d]) // pass along data from parent to child
+    .join("circle")
+    .attr("r", radius)
+
+  // add text into group
+  dots.selectAll("text")
+    .data(d => [d]) // pass along data from parent to child
+    .join("text")
+    .attr("text-anchor", "end")
+    .text(d => `${formatDate(d.year)}: ${formatBillions(d.population)} `)
+
+  // specify line generator function
+  const lineGen = d3.line()
+    .x(d => xScale(d.year))
+    .y(d => yScale(d.population))
+
+  // + DRAW LINE AND/OR AREA
+  svg.selectAll(".line")
+    .data([filteredData]) // data needs to take an []
+    .join("path")
+    .attr("class", 'line')
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .transition()
+    .duration(1000)
+    .attr("d", d => lineGen(d))
 }
