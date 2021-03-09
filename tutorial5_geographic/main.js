5974ecb5e60a1c27ca096269db28a305a17816fb
auchers <aucher@two-n.com>
2021-03-08 20:44:04 -0500

    update class code

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -14,7 +14,7 @@ let svg;
  * APPLICATION STATE
  * */
 let state = {
-  // + SET UP STATE
+  geojson: null,
 };
 
 /**
@@ -22,10 +22,11 @@ let state = {
  * Using a Promise.all([]), we can load more than one dataset at a time
  * */
 Promise.all([
-  d3.json("PATH_TO_YOUR_GEOJSON"),
-  d3.csv("PATH_TO_ANOTHER_DATASET", d3.autoType),
+  d3.json("../data/usState.json"),
+  // d3.csv("PATH_TO_ANOTHER_DATASET", d3.autoType),
 ]).then(([geojson, otherData]) => {
   // + SET STATE WITH DATA
+  state.geojson = geojson
   console.log("state: ", state);
   init();
 });
@@ -35,6 +36,17 @@ Promise.all([
  * this will be run *one time* when the data finishes loading in
  * */
 function init() {
+  // long/lat => x/y
+  const projection = d3.geoAlbersUsa()
+    .fitSize([width, height], state.geojson)
+
+  const colorScale = d3.scaleSequential(d3.interpolateBlues)
+    .domain(d3.extent(state.geojson.features, d=> d.properties.AWATER))
+
+  const pathFunction = d3.geoPath(projection)
+
+  const graduateCenterCoords = [{long: -73.984, lat:40.7486}] // long, lat
+
   // create an svg element in our main `d3-container` element
   svg = d3
     .select("#d3-container")
@@ -42,11 +54,28 @@ function init() {
     .attr("width", width)
     .attr("height", height);
 
-  // + SET UP PROJECTION
-  // + SET UP GEOPATH
+    // base layer of states
+  svg.selectAll("path")
+    .data(state.geojson.features)
+    .join("path")
+    .attr("stroke", "black")
+    .attr("fill", d => {
+      return colorScale(d.properties.AWATER)
+    })
+    .attr("d", pathFunction) // lat/long-> x/y
 
-  // + DRAW BASE MAP PATH
-  // + ADD EVENT LISTENERS (if you want)
+  // cuny circle
+  svg.selectAll("circle")
+    .data(graduateCenterCoords)
+    .join("circle")
+    .attr("fill", "red")
+    .attr("r", 3)
+    .attr("transform", d => {
+      console.log("d", d)
+      const point = projection([d.long, d.lat])
+      console.log("point", point)
+      return `translate(${point[0]}, ${point[1]})`
+    })
 
   draw(); // calls the draw function
 }
