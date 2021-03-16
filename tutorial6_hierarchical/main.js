37c3fb66ae7d0f0752deab1a1c6a887a2abee9c4
auchers <aucher@two-n.com>
2021-03-15 21:00:41 -0400

    update class code

--- tutorial6_hierarchical/main.js
+++ tutorial6_hierarchical/main.js
@@ -28,6 +28,8 @@ d3.json("../../data/flare.json", d3.autotype).then(data => {
  * this will be run *one time* when the data finishes loading in
  * */
 function init() {
+  const colorScale = d3.scaleOrdinal(d3.schemeSet3)
+
   const container = d3.select("#d3-container").style("position", "relative");
 
   svg = container
@@ -35,15 +37,51 @@ function init() {
     .attr("width", width)
     .attr("height", height);
 
-  // + INITIALIZE TOOLTIP IN YOUR CONTAINER ELEMENT
+  tooltip = container
+    .append("div")
+    .style("position", "absolute")
+    .style("top", 0)
+    .style("left", 0)
+    .style("background-color", "white")
 
-  // + CREATE YOUR ROOT HIERARCHY NODE
+  const root = d3.hierarchy(state.data)
+    .sum(d => d.value)
 
-  // + CREATE YOUR LAYOUT GENERATOR
+  const treeLayout = d3.treemap()
+    .size([width, height])
+    .padding(2)
 
-  // + CALL YOUR LAYOUT FUNCTION ON YOUR ROOT DATA
+  treeLayout(root)
 
-  // + CREATE YOUR GRAPHICAL ELEMENTS
+  const leaves = root.leaves()
+
+  const leafGroup = svg.selectAll("g")
+    .data(leaves)
+    .join("g")
+    .attr("transform", d => `translate(${d.x0}, ${d.y0})`)
+
+  // append rect
+  leafGroup.append("rect")
+    .attr("fill", d => {
+      const level1Anscestor = d.ancestors().find(a => a.depth === 1)
+      return colorScale(level1Anscestor.data.name)
+    })
+    .attr("stroke", "red")
+    .attr("width", d => d.x1 - d.x0)
+    .attr("height", d => d.y1 - d.y0)
+
+  //append text
+  leafGroup.append("text")
+    .attr("dy", "1em")
+    .text(d => d.data.name)
+
+
+  leafGroup.on("mouseenter", (event, d) => {
+    state.hoverLeaf = d.data.name
+    state.hoverPositionX = d.x0
+    state.hoverPositionY = d.y0
+    draw()
+  })
 
   draw(); // calls the draw function
 }
@@ -53,5 +91,10 @@ function init() {
  * we call this everytime there is an update to the data/state
  * */
 function draw() {
-  // + UPDATE TOOLTIP
+
+  tooltip
+    .style("transform", `translate(${state.hoverPositionX}px, ${state.hoverPositionY}px)`)
+    .html(`
+  <div>${state.hoverLeaf}</div>
+  `)
 }
